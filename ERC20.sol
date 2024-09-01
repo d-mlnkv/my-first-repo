4629e75cdc5757f10929d12dadade757f92a8920
d-mlnkv <154833295+d-mlnkv@users.noreply.github.com>
2024-09-01 23:34:31 +0200

    Update ERC20.sol

--- ERC20.sol
+++ ERC20.sol
@@ -1,312 +1,169 @@
-// SPDX-License-Identifier: MIT
-// OpenZeppelin Contracts (last updated v5.0.0) (token/ERC20/ERC20.sol)
+let Auction = artifacts.require("./Auction.sol");
 
-pragma solidity ^0.8.20;
+let auctionInstance;
 
-import {IERC20} from "./IERC20.sol";
-import {IERC20Metadata} from "./extensions/IERC20Metadata.sol";
-import {Context} from "../../utils/Context.sol";
-import {IERC20Errors} from "../../interfaces/draft-IERC6093.sol";
+contract('AuctionContract', function (accounts) {
+  //accounts[0] is the default account
+  //Test case 1
+  it("Contract deployment", function() {
+    //Fetching the contract instance of our smart contract
+    return Auction.deployed().then(function (instance) {
+      //We save the instance in a gDlobal variable and all smart contract functions are called using this
+      auctionInstance = instance;
+      assert(auctionInstance !== undefined, 'Auction contract should be defined');
+    });
+  });
 
-/**
- * @dev Implementation of the {IERC20} interface.
- *
- * This implementation is agnostic to the way tokens are created. This means
- * that a supply mechanism has to be added in a derived contract using {_mint}.
- *
- * TIP: For a detailed writeup see our guide
- * https://forum.openzeppelin.com/t/how-to-implement-erc20-supply-mechanisms/226[How
- * to implement supply mechanisms].
- *
- * The default value of {decimals} is 18. To change this, you should override
- * this function so it returns a different value.
- *
- * We have followed general OpenZeppelin Contracts guidelines: functions revert
- * instead returning `false` on failure. This behavior is nonetheless
- * conventional and does not conflict with the expectations of ERC-20
- * applications.
- */
-abstract contract ERC20 is Context, IERC20, IERC20Metadata, IERC20Errors {
-    mapping(address account => uint256) private _balances;
+  //Sample Test Case
+  it("Should set bidders", function() {
+    return auctionInstance.register({from:accounts[1]}).then(function(result) {
+        return auctionInstance.getPersonDetails(0);
+    }).then(function(result) {
+      assert.equal(result[2], accounts[1], 'bidder address set');
+    })
+  });
 
-    mapping(address account => mapping(address spender => uint256)) private _allowances;
+  //Test Case for checking if the bid is more than the token amount
+  it("Should NOT allow to bid more than remaining tokens", function() {
+    /**********
+    TASK 1:   Call bid method from accounts[1] of Auction.sol using auctionInstance and
+    pass itemId=0, count=6 as arguments
+    HINT:     To make a function call from account 1 use {from: accounts[1]} as an extra argument
+    ***********/
+    return auctionInstance.bid(0, 6, {from: accounts[1]} /*<CODE HERE>*/
+    return auctionInstance.bid(0, 6, {from: accounts[1]})
+    .then(function (result) {
+      /*
+      We are testing for a negative condition and hence this particular block will not have executed if our test case was correct. If this part is executed then we throw an error and catch the error to assert false
+      */
+      throw("Failed to check remaining tokens less than count");
+    }).catch(function (e) {
+      var a = e.toString();
+      if(e === "Failed to check remaining tokens less than count") {
+        /**********
+        TASK 2: This is the error which we had thrown. Should you assert true or false?
+        HINT:   Use assert(false) to assert false
+                Use assert(true) to assert true
+        ***********/
+	assert(false);
+      } else {
+        /**********
+        TASK 3: assert the opposite here
+        ***********/
+	assert(true);
+      }
+    })
+  });
 
-    uint256 private _totalSupply;
+  //Modifier Checking
+  it("Should NOT allow non owner to reveal winners", function() {
+    /**********
+    TASK 4: Call revealWinners from account 1
+    ***********/
+     return auctionInstance.revealWinners({from: accounts[1]}/*<CODE HERE>*/
+     .then(function (instance) {
+       /*
+       We are testing for a negative condition and hence this particular block will not have executed if our test case was correct. If this part is executed then we throw an error and catch the error to assert false
+       */
+       throw("Failed to check owner in reveal winners");
+     }).catch(function (e) {
+       if(e === "Failed to check owner in reveal winners") {
+         /**********
+         TASK 5: This is the error which we had thrown. Should you assert true or false?
+         HINT:   Use assert(false) to assert false
+                 Use assert(true) to assert true
+         ***********/
+		assert(false);
+         /*<CODE HERE>*/
+       } else {
+         /**********
+         TASK 6: assert the opposite here
+         ***********/
+		assert(true);
+         /*<CODE HERE>*/
+       }
+     })
+   });
 
-    string private _name;
-    string private _symbol;
 
-    /**
-     * @dev Sets the values for {name} and {symbol}.
-     *
-     * All two of these values are immutable: they can only be set once during
-     * construction.
-     */
-    constructor(string memory name_, string memory symbol_) {
-        _name = name_;
-        _symbol = symbol_;
-    }
-
-    /**
-     * @dev Returns the name of the token.
-     */
-    function name() public view virtual returns (string memory) {
-        return _name;
-    }
-
-    /**
-     * @dev Returns the symbol of the token, usually a shorter version of the
-     * name.
-     */
-    function symbol() public view virtual returns (string memory) {
-        return _symbol;
-    }
-
-    /**
-     * @dev Returns the number of decimals used to get its user representation.
-     * For example, if `decimals` equals `2`, a balance of `505` tokens should
-     * be displayed to a user as `5.05` (`505 / 10 ** 2`).
-     *
-     * Tokens usually opt for a value of 18, imitating the relationship between
-     * Ether and Wei. This is the default value returned by this function, unless
-     * it's overridden.
-     *
-     * NOTE: This information is only used for _display_ purposes: it in
-     * no way affects any of the arithmetic of the contract, including
-     * {IERC20-balanceOf} and {IERC20-transfer}.
-     */
-    function decimals() public view virtual returns (uint8) {
-        return 18;
-    }
-
-    /**
-     * @dev See {IERC20-totalSupply}.
-     */
-    function totalSupply() public view virtual returns (uint256) {
-        return _totalSupply;
-    }
-
-    /**
-     * @dev See {IERC20-balanceOf}.
-     */
-    function balanceOf(address account) public view virtual returns (uint256) {
-        return _balances[account];
-    }
-
-    /**
-     * @dev See {IERC20-transfer}.
-     *
-     * Requirements:
-     *
-     * - `to` cannot be the zero address.
-     * - the caller must have a balance of at least `value`.
-     */
-    function transfer(address to, uint256 value) public virtual returns (bool) {
-        address owner = _msgSender();
-        _transfer(owner, to, value);
-        return true;
-    }
-
-    /**
-     * @dev See {IERC20-allowance}.
-     */
-    function allowance(address owner, address spender) public view virtual returns (uint256) {
-        return _allowances[owner][spender];
-    }
-
-    /**
-     * @dev See {IERC20-approve}.
-     *
-     * NOTE: If `value` is the maximum `uint256`, the allowance is not updated on
-     * `transferFrom`. This is semantically equivalent to an infinite approval.
-     *
-     * Requirements:
-     *
-     * - `spender` cannot be the zero address.
-     */
-    function approve(address spender, uint256 value) public virtual returns (bool) {
-        address owner = _msgSender();
-        _approve(owner, spender, value);
-        return true;
-    }
-
-    /**
-     * @dev See {IERC20-transferFrom}.
-     *
-     * Skips emitting an {Approval} event indicating an allowance update. This is not
-     * required by the ERC. See {xref-ERC20-_approve-address-address-uint256-bool-}[_approve].
-     *
-     * NOTE: Does not update the allowance if the current allowance
-     * is the maximum `uint256`.
-     *
-     * Requirements:
-     *
-     * - `from` and `to` cannot be the zero address.
-     * - `from` must have a balance of at least `value`.
-     * - the caller must have allowance for ``from``'s tokens of at least
-     * `value`.
-     */
-    function transferFrom(address from, address to, uint256 value) public virtual returns (bool) {
-        address spender = _msgSender();
-        _spendAllowance(from, spender, value);
-        _transfer(from, to, value);
-        return true;
-    }
-
-    /**
-     * @dev Moves a `value` amount of tokens from `from` to `to`.
-     *
-     * This internal function is equivalent to {transfer}, and can be used to
-     * e.g. implement automatic token fees, slashing mechanisms, etc.
-     *
-     * Emits a {Transfer} event.
-     *
-     * NOTE: This function is not virtual, {_update} should be overridden instead.
-     */
-    function _transfer(address from, address to, uint256 value) internal {
-        if (from == address(0)) {
-            revert ERC20InvalidSender(address(0));
-        }
-        if (to == address(0)) {
-            revert ERC20InvalidReceiver(address(0));
-        }
-        _update(from, to, value);
-    }
-
-    /**
-     * @dev Transfers a `value` amount of tokens from `from` to `to`, or alternatively mints (or burns) if `from`
-     * (or `to`) is the zero address. All customizations to transfers, mints, and burns should be done by overriding
-     * this function.
-     *
-     * Emits a {Transfer} event.
-     */
-    function _update(address from, address to, uint256 value) internal virtual {
-        if (from == address(0)) {
-            // Overflow check required: The rest of the code assumes that totalSupply never overflows
-            _totalSupply += value;
-        } else {
-            uint256 fromBalance = _balances[from];
-            if (fromBalance < value) {
-                revert ERC20InsufficientBalance(from, fromBalance, value);
-            }
-            unchecked {
-                // Overflow not possible: value <= fromBalance <= totalSupply.
-                _balances[from] = fromBalance - value;
-            }
-        }
-
-        if (to == address(0)) {
-            unchecked {
-                // Overflow not possible: value <= totalSupply or value <= fromBalance <= totalSupply.
-                _totalSupply -= value;
-            }
-        } else {
-            unchecked {
-                // Overflow not possible: balance + value is at most totalSupply, which we know fits into a uint256.
-                _balances[to] += value;
-            }
-        }
-
-        emit Transfer(from, to, value);
-    }
-
-    /**
-     * @dev Creates a `value` amount of tokens and assigns them to `account`, by transferring it from address(0).
-     * Relies on the `_update` mechanism
-     *
-     * Emits a {Transfer} event with `from` set to the zero address.
-     *
-     * NOTE: This function is not virtual, {_update} should be overridden instead.
-     */
-    function _mint(address account, uint256 value) internal {
-        if (account == address(0)) {
-            revert ERC20InvalidReceiver(address(0));
-        }
-        _update(address(0), account, value);
-    }
-
-    /**
-     * @dev Destroys a `value` amount of tokens from `account`, lowering the total supply.
-     * Relies on the `_update` mechanism.
-     *
-     * Emits a {Transfer} event with `to` set to the zero address.
-     *
-     * NOTE: This function is not virtual, {_update} should be overridden instead
-     */
-    function _burn(address account, uint256 value) internal {
-        if (account == address(0)) {
-            revert ERC20InvalidSender(address(0));
-        }
-        _update(account, address(0), value);
-    }
-
-    /**
-     * @dev Sets `value` as the allowance of `spender` over the `owner` s tokens.
-     *
-     * This internal function is equivalent to `approve`, and can be used to
-     * e.g. set automatic allowances for certain subsystems, etc.
-     *
-     * Emits an {Approval} event.
-     *
-     * Requirements:
-     *
-     * - `owner` cannot be the zero address.
-     * - `spender` cannot be the zero address.
-     *
-     * Overrides to this logic should be done to the variant with an additional `bool emitEvent` argument.
-     */
-    function _approve(address owner, address spender, uint256 value) internal {
-        _approve(owner, spender, value, true);
-    }
-
-    /**
-     * @dev Variant of {_approve} with an optional flag to enable or disable the {Approval} event.
-     *
-     * By default (when calling {_approve}) the flag is set to true. On the other hand, approval changes made by
-     * `_spendAllowance` during the `transferFrom` operation set the flag to false. This saves gas by not emitting any
-     * `Approval` event during `transferFrom` operations.
-     *
-     * Anyone who wishes to continue emitting `Approval` events on the`transferFrom` operation can force the flag to
-     * true using the following override:
-     *
-     * ```solidity
-     * function _approve(address owner, address spender, uint256 value, bool) internal virtual override {
-     *     super._approve(owner, spender, value, true);
-     * }
-     * ```
-     *
-     * Requirements are the same as {_approve}.
-     */
-    function _approve(address owner, address spender, uint256 value, bool emitEvent) internal virtual {
-        if (owner == address(0)) {
-            revert ERC20InvalidApprover(address(0));
-        }
-        if (spender == address(0)) {
-            revert ERC20InvalidSpender(address(0));
-        }
-        _allowances[owner][spender] = value;
-        if (emitEvent) {
-            emit Approval(owner, spender, value);
-        }
-    }
-
-    /**
-     * @dev Updates `owner` s allowance for `spender` based on spent `value`.
-     *
-     * Does not update the allowance value in case of infinite allowance.
-     * Revert if not enough allowance is available.
-     *
-     * Does not emit an {Approval} event.
-     */
-    function _spendAllowance(address owner, address spender, uint256 value) internal virtual {
-        uint256 currentAllowance = allowance(owner, spender);
-        if (currentAllowance != type(uint256).max) {
-            if (currentAllowance < value) {
-                revert ERC20InsufficientAllowance(spender, currentAllowance, value);
-            }
-            unchecked {
-                _approve(owner, spender, currentAllowance - value, false);
-            }
-        }
-    }
-}
+  it("Should set winners", function() {
+    /**********
+    TASK 7: Call register function from account 2
+    ***********/
+    return auctionInstance.register({from: accounts[2])}/*<CODE HERE>*/
+    .then(function(result) {
+      /**********
+      TASK 8: Call register function from account 3
+      ***********/
+        return auctionInstance.register({from: accounts[3] /*<CODE HERE>*/
+    }).then(function() {
+      /**********
+      TASK 9: Call register function from account 4
+      ***********/
+        return auctionInstance.register({from: accounts[4]/*<CODE HERE>*/
+    }).then(function() {
+      /**********
+      TASK 10: Call bid method from accounts[2] of Auction.sol using auctionInstance and
+      pass itemId=0, count=5 as arguments
+      ***********/
+        return auctionInstance.bid(0, 5, {from: accounts[2] /*<CODE HERE>*/
+    }).then(function() {
+      /**********
+      TASK 11: Call bid method from accounts[3] of Auction.sol using auctionInstance and
+      pass itemId=1, count=5 as arguments
+      ***********/
+        return auctionInstance.bid(1, 5, {from: accounts[3] /*<CODE HERE>*/
+    }).then(function() {
+      /**********
+      TASK 12: Call bid method from accounts[4] of Auction.sol using auctionInstance and
+      pass itemId=2, count=5 as arguments
+      ***********/
+        return auctionInstance.bid(2, 5, {from: accounts[4] /*<CODE HERE>*/
+    }).then(function() {
+      /**********
+      TASK 13: Call revealWinners function from accounts[0]
+      ***********/
+        return auctionInstance.revealWinners({from: accounts[0]/*<CODE HERE>*/
+    }).then(function() {
+      /**********
+      TASK 14: call winners function from accounts[0] to get the winner of item id 0
+      ***********/
+        return auctionInstance.winners(0, {from: accounts[0] /*<CODE HERE>*/
+    }).then(function(result) {
+      /**********
+      TASK 15:  assert to see if the winner address is not the default address
+      HINT:     Default address is '0x0000000000000000000000000000000000000000'
+                Use notEqual method of assert
+                Parameters for notEqual : (result, default address , message);
+      ***********/
+      assert.notEqual: (result, '0x0000000000000000000000000000000000000000', "Winner address is not the default address");
+      /*<CODE HERE>*/
+      /**********
+      TASK 16: call winners function from accounts[0] to get the winner of item id 1
+      ***********/
+      return auctionInstance.revealWinners(1, {from: accounts[0]/*<CODE HERE>*/
+    }).then(function(result) {
+      /**********
+      TASK 17:  assert to see if the winner address is not the default address
+      HINT:     Default address is '0x0000000000000000000000000000000000000000'
+                Use notEqual method of assert
+                Parameters for notEqual : (result, default address , message);
+      ***********/
+      assert.notEqual: (result, '0x0000000000000000000000000000000000000000', "Winner address is not the default address");
+      /*<CODE HERE>*/
+      /**********
+      TASK 18: Call winners function from account 3 to get the winner of item id 2
+      ***********/
+      return auctionInstance.winners(2, {from: accounts[3] /*<CODE HERE>*/
+    }).then(function(result) {
+      /**********
+      TASK 19:  assert to see if the winner address is not the default address
+      HINT:     Default address is '0x0000000000000000000000000000000000000000'
+                Use notEqual method of assert
+                Parameters for notEqual : (result, default address , message);
+      ***********/
+	assert.notEqual: (result, '0x0000000000000000000000000000000000000000', "Winner address is not the default address");
+      /*<CODE HERE>*/
+    })
+  })
+});
